612cf5015e937b569804e72799b37358c6f250f4
JaviUga <j.ugarrio.duro@gmail.com>
2024-12-18 10:34:22 +0100

    mi primer commit

---
+++ src/main/java/com/company/competenzia/CompetenziaApplication.java
@@ -0,0 +1,13 @@
+package com.company.competenzia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetenziaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetenziaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/competenzia/CompetenziaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.competenzia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetenziaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
