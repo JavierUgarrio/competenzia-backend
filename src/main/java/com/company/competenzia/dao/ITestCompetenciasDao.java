1e09716e12511bd5c4f9b01a5857ac2c99a80743
JaviUga <j.ugarrio.duro@gmail.com>
2025-04-08 09:17:23 +0200

    añadir comentarios en el proces de Empleados

--- src/main/java/com/company/competenzia/dao/ITestCompetenciasDao.java
+++ src/main/java/com/company/competenzia/dao/ITestCompetenciasDao.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.company.competenzia.modelo.TestCompetencia;
 
 
-
+//Interfaz que extiende CrudRepository para proporcionar métodos CRUD básicos
 public interface ITestCompetenciasDao extends CrudRepository<TestCompetencia, Long> {
 
 	
--- src/main/java/com/company/competenzia/respuesta/RespuestaRest.java
+++ src/main/java/com/company/competenzia/respuesta/RespuestaRest.java
@@ -3,8 +3,16 @@ package com.company.competenzia.respuesta;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/*
+El objetivo de esta clase es crear una estructura que sea sencilla de gestionar, donde te da la informacion del envio de datos
+
+creando la variable metadata como ArrayList, donde dentro hay un hashmapa que con la clave y el valor te va a decir el estado de envio de los datos 
+
+*/
+
 public class RespuestaRest {
 
+	// Lista que almacena mapas (HashMap) con información de metadatos (tipo, código, estado)
 	private ArrayList<HashMap<String , String>> metadata = new ArrayList<>();
 	
 	//GET & SET
@@ -13,8 +21,10 @@ public class RespuestaRest {
 		return metadata;
 	}
 
+	// Añade un nuevo conjunto de metadatos a la lista (tipo, código, estado).
 	public void setMetadata(String tipo, String codigo, String estado) {
 		
+		  // Crea un mapa con los valores tipo, código y estado.
 		HashMap<String, String> mapeo = new HashMap<String, String>();
 		mapeo.put("tipo", tipo);
 		mapeo.put(codigo, codigo);
@@ -25,8 +35,3 @@ public class RespuestaRest {
 	}
 }
 
-/*
- El objetivo de esta clase es crear una estructura que sea sencilla de gestionar, donde te da la informacion del envio de datos
- 
- creando la variable metadata como ArrayList, donde dentro hay un hashmapa que con la clave y el valor te va a decir el estado de envio de los datos 
-*/
